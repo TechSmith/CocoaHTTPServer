666ca8357deaca50c2ad989148db6a59ae28c421
Jason Eagleston <j.eagleston@techsmith.com>
2019-04-04 08:12:39 -0400

    fix a bunch of implicit self warnings

--- Core/WebSocket.m
+++ Core/WebSocket.m
@@ -201,7 +201,7 @@ static inline NSUInteger WS_PAYLOAD_LENGTH(UInt8 frame)
 	__block id result = nil;
 	
 	dispatch_sync(websocketQueue, ^{
-		result = delegate;
+		result = self.delegate;
 	});
 	
 	return result;
@@ -210,7 +210,7 @@ static inline NSUInteger WS_PAYLOAD_LENGTH(UInt8 frame)
 - (void)setDelegate:(id)newDelegate
 {
 	dispatch_async(websocketQueue, ^{
-		delegate = newDelegate;
+		self.delegate = newDelegate;
 	});
 }
 
@@ -229,10 +229,10 @@ static inline NSUInteger WS_PAYLOAD_LENGTH(UInt8 frame)
 	
 	dispatch_async(websocketQueue, ^{ @autoreleasepool {
 		
-		if (isStarted) return;
-		isStarted = YES;
+		if (self->isStarted) return;
+		self->isStarted = YES;
 		
-		if (isVersion76)
+		if (self->isVersion76)
 		{
 			[self readRequestBody];
 		}
@@ -255,7 +255,7 @@ static inline NSUInteger WS_PAYLOAD_LENGTH(UInt8 frame)
 	
 	dispatch_async(websocketQueue, ^{ @autoreleasepool {
 		
-		[asyncSocket disconnect];
+		[self->asyncSocket disconnect];
 	}});
 }
 
